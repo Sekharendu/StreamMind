8eb7d8dff46003a27065429d2bf59edb869d5fd2
Sekharendu <sekharendudey12@gmail.com>
2026-08-15 17:25:34 +0530

    feat: storing gemini and user responses to db and exposed a GET /chats/:id/messages path

--- src/routes/chat.ts
+++ src/routes/chat.ts
@@ -1,11 +1,17 @@
 import "dotenv/config";
 import {geminiAgent} from '../services/gemini.js'
+import { pool } from "../services/db.js";
 import type { FastifyRequest, FastifyReply, FastifyInstance, FastifyPluginOptions } from 'fastify';
 
 type ReqType = {
     Body: {
         query : string;
     };
+}
+type GETReqType = {
+    Params:{
+        id: string;
+    }
 }
 async function chatRoute(fastify: FastifyInstance, options: FastifyPluginOptions){
     fastify.post<ReqType>('/chat',async (request,reply)=>{
@@ -34,18 +40,24 @@ async function chatRoute(fastify: FastifyInstance, options: FastifyPluginOptions
             "no"
         );
          try {
-
+            const chatObj = await pool.query("INSERT INTO chats(title) VALUES($1) RETURNING id" , [request.body.query.slice(0,50)]);
+            var chatId = chatObj.rows[0].id;
+            // save user mssg to db
+            await pool.query("INSERT INTO messages(chat_id, role, content) VALUES($1,$2,$3)",[chatId,"user",request.body.query]);
+            var gemResponseInChunks=""; 
             for await (const chunk of geminiAgent(request.body.query)) {
-
+                //chunk = only the text message like, chunk = "Hi!! i am good"
                 reply.raw.write(
                     `event: chunk\n` +
                     `data: ${JSON.stringify({
                         text: chunk,
                     })}\n\n`
                 );
-
+                gemResponseInChunks+=chunk as unknown as string;
             }
-
+            //save geminis response to db
+            await pool.query("INSERT INTO messages(chat_id, role, content) VALUES($1,$2,$3)", [chatId, "assistant", gemResponseInChunks]);
+            
             // Tell client generation completed
             reply.raw.write(
                 `event: done\n` +
@@ -53,25 +65,29 @@ async function chatRoute(fastify: FastifyInstance, options: FastifyPluginOptions
                     success: true,
                 })}\n\n`
             );
-
         } catch (error) {
-
             fastify.log.error(error);
-
+            const errorMessage = "Failed to generate response";
             // IMPORTANT:
             // We cannot send HTTP 500 here because
             // the SSE response has already started.
-
+            await pool.query("INSERT INTO messages(chat_id, role, content) VALUES($1,$2,$3)", [chatId, "assistant", `[Error]: ${errorMessage}`]);
             reply.raw.write(
                 `event: error\n` +
                 `data: ${JSON.stringify({
-                    error: "Failed to generate response",
+                    error: errorMessage,
                 })}\n\n`
             );
-
+            
         } finally {
             reply.raw.end();
         }
+    })
+    fastify.get<GETReqType>('/chats/:id/messages', async (request,reply)=>{
+        const paramsObj = request.params;
+        const chatId = String(paramsObj?.id);
+        const userQuery = await pool.query("SELECT * FROM messages WHERE chat_id = $1", [chatId]);
+        reply.send(userQuery.rows);
     })
     return;
 }
