5c0936a6ebe2213640d22dcbda435d4f8b02cd07
Sekharendu <sekharendudey12@gmail.com>
2026-09-09 18:53:14 +0530

    feat: create document_chunks table schema to hold chunks and vector together

--- schema.sql
+++ schema.sql
@@ -1,3 +1,5 @@
+CREATE EXTENSION IF NOT EXISTS vector;
+
 CREATE TABLE IF NOT EXISTS chats(
     id SERIAL PRIMARY KEY,
     title TEXT NOT NULL,
@@ -10,5 +12,14 @@ CREATE TABLE IF NOT EXISTS messages(
     role TEXT NOT NULL,
     content TEXT NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS document_chunks(
+    chunk_id UUID PRIMARY KEY,
+    document_id TEXT NOT NULL,
+    source TEXT NOT NULL,
+    chunk_index INTEGER NOT NULL,
+    content TEXT NOT NULL,
+    embedding vector(768) NOT NULL
 );
 CREATE INDEX IF NOT EXISTS message_chat_id_idx ON messages(chat_id);
